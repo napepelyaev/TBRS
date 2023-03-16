843baeaa5c9ae25885c3e8372e8bbff794aa849f
Николай Пепеляев <napepelyaev@gmail.com>
2023-03-16 21:47:56 +0500

    Add creating and deletion of arrays

--- TBRS_3/TBRS_3.cpp
+++ TBRS_3/TBRS_3.cpp
@@ -1,6 +1,55 @@
 ﻿#include <iostream>
+// размер стороны
+const int size = 500;
+
+void print(int** array);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+#pragma region Инициализация массивов
+    int** A = new int* [size];
+    int** B = new int* [size];
+    int** C = new int* [size];
+
+    for (int i = 0; i < size; i++) {
+        A[i] = new int[size];
+        B[i] = new int[size];
+        C[i] = new int[size];
+    }
+#pragma endregion
+
+#pragma region Заполнение массивов
+    // перебор строк
+    for (int i = 0; i < size; i++) {
+        // перебор столбцов
+        for (int j = 0; j < size; j++) {
+            A[i][j] = rand() % 500 - 250;
+            B[i][j] = rand() % 500 - 250;
+        }
+    }
+#pragma endregion
+
+    print(A);
+
+#pragma region Удаление массивов
+    for (int i = 0; i < size; i++) {
+        delete[] A[i];
+        delete[] B[i];
+        delete[] C[i];
+    }
+
+    delete[] A;
+    delete[] B;
+    delete[] C;
+#pragma endregion
+}
+
+// Вывод матрицы с размером [size]
+void print(int** array) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            std::cout << array[i][j] << "\t";
+        }
+        std::cout << std::endl;
+    }
 }
