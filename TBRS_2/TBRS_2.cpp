61ea3666f0a6c719c95e2bee1e8e2d840f79caf2
Николай Пепеляев <napepelyaev@gmail.com>
2023-03-29 17:24:49 +0500

    Changes in numeration of LW

---
+++ TBRS_2/TBRS_2.cpp
@@ -0,0 +1,202 @@
+﻿#include <iostream>
+#include <chrono>
+#include <omp.h>
+// размер стороны
+const int size = 500;
+
+void print(int** array);
+inline std::chrono::microseconds multiplyLinearIJK(int** first, int** second, int** res);
+inline std::chrono::microseconds multiplyOpenMP(int** first, int** second, int** res);
+inline void resetMatrix(int** matrix);
+inline std::chrono::microseconds multiplyLinearKIJ(int** first, int** second, int** res);
+
+inline std::chrono::microseconds multiplyLinearIKJ(int** first, int** second, int** res)
+{
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < size; i++) {
+        for (int k = 0; k < size; k++) {
+            for (int j = 0; j < size; j++) {
+                res[i][j] += first[i][k] * second[k][j];
+            }
+        }
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    return std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+}
+
+inline std::chrono::microseconds multiplyLinearJIK(int** first, int** second, int** res)
+{
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (int j = 0; j < size; j++) {
+        for (int i = 0; i < size; i++) {
+            for (int k = 0; k < size; k++) {
+                res[i][j] += first[i][k] * second[k][j];
+            }
+        }
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    return std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+}
+
+inline std::chrono::microseconds multiplyLinearJKI(int** first, int** second, int** res)
+{
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (int j = 0; j < size; j++) {
+        for (int k = 0; k < size; k++) {
+            for (int i = 0; i < size; i++) {
+                res[i][j] += first[i][k] * second[k][j];
+            }
+        }
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    return std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+}
+
+
+inline std::chrono::microseconds multiplyLinearKJI(int** first, int** second, int** res)
+{
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (int k = 0; k < size; k++) {
+        for (int j = 0; j < size; j++) {
+            for (int i = 0; i < size; i++) {
+                res[i][j] += first[i][k] * second[k][j];
+            }
+        }
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    return std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+}
+
+int main()
+{
+#pragma region Инициализация массивов
+    int** A = new int* [size];
+    int** B = new int* [size];
+    int** C = new int* [size];
+
+    for (int i = 0; i < size; i++) {
+        A[i] = new int[size];
+        B[i] = new int[size];
+        C[i] = new int[size];
+    }
+#pragma endregion
+
+#pragma region Заполнение массивов
+    // перебор строк
+    for (int i = 0; i < size; i++) {
+        // перебор столбцов
+        for (int j = 0; j < size; j++) {
+            A[i][j] = rand() % 500 - 250;
+            B[i][j] = rand() % 500 - 250;
+        }
+    }
+#pragma endregion
+
+    std::cout << "OpenMP:\t\t" << multiplyOpenMP(A, B, C).count() << " microseconds" << std::endl;
+    resetMatrix(C);
+    std::cout << "Sequential:\t" << multiplyLinearIJK(A, B, C).count() << " microseconds" << std::endl;
+    resetMatrix(C);
+    /*std::cout << "IKJ result:\t" << multiplyLinearIKJ(A, B, C).count() << " microseconds" << std::endl;
+    resetMatrix(C);
+    std::cout << "JIK result:\t" << multiplyLinearJIK(A, B, C).count() << " microseconds" << std::endl;
+    resetMatrix(C);
+    std::cout << "JKI result:\t" << multiplyLinearJKI(A, B, C).count() << " microseconds" << std::endl;
+    resetMatrix(C);*/
+    std::cout << "Optimal seq:\t" << multiplyLinearKIJ(A, B, C).count() << " microseconds" << std::endl;
+    resetMatrix(C);
+    /*std::cout << "KJI result:\t" << multiplyLinearKJI(A, B, C).count() << " microseconds" << std::endl;
+    resetMatrix(C);*/
+
+#pragma region Удаление массивов
+    for (int i = 0; i < size; i++) {
+        delete[] A[i];
+        delete[] B[i];
+        delete[] C[i];
+    }
+
+    delete[] A;
+    delete[] B;
+    delete[] C;
+#pragma endregion
+}
+
+// Вывод матрицы с размером [size]
+void print(int** array) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            std::cout << array[i][j] << "\t";
+        }
+        std::cout << std::endl;
+    }
+}
+
+inline std::chrono::microseconds multiplyLinearKIJ(int** first, int** second, int** res)
+{
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (int k = 0; k < size; k++) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                res[i][j] += first[i][k] * second[k][j];
+            }
+        }
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    return std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+}
+
+inline std::chrono::microseconds multiplyLinearIJK(int** first, int** second, int** res)
+{
+    auto start = std::chrono::high_resolution_clock::now();
+    
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            for (int k = 0; k < size; k++) {
+                res[i][j] += first[i][k] * second[k][j];
+            }
+        }
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    return std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+}
+
+inline std::chrono::microseconds multiplyOpenMP(int** first, int** second, int** res)
+{
+    int threadsNum = 2;
+    omp_set_num_threads(threadsNum);
+    int i, j, k;
+    auto start = std::chrono::high_resolution_clock::now();
+#pragma omp parallel for shared(first, second, res) private(i, j, k)
+
+    
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size; j++) {
+            for (k = 0; k < size; k++) {
+                res[i][j] += first[i][k] * second[k][j];
+            }
+        }   
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    return std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+}
+
+inline void resetMatrix(int** matrix) {
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            matrix[i][j] = 0;
+        }
+    }
+}
--- TBRS_3/TBRS_3.cpp
+++ TBRS_3/TBRS_3.cpp
@@ -1,202 +1,121 @@
 ﻿#include <iostream>
-#include <chrono>
-#include <omp.h>
-// размер стороны
-const int size = 500;
+#include <chrono>  
+#include <omp.h>   
 
-void print(int** array);
-inline std::chrono::microseconds multiplyLinearIJK(int** first, int** second, int** res);
-inline std::chrono::microseconds multiplyOpenMP(int** first, int** second, int** res);
-inline void resetMatrix(int** matrix);
-inline std::chrono::microseconds multiplyLinearKIJ(int** first, int** second, int** res);
+const int N = 2000;
+void initArrays(double** A, double* b, double* x);
+void eraseArrays(double** A, double* b, double* x);
 
-inline std::chrono::microseconds multiplyLinearIKJ(int** first, int** second, int** res)
-{
-    auto start = std::chrono::high_resolution_clock::now();
-
-    for (int i = 0; i < size; i++) {
-        for (int k = 0; k < size; k++) {
-            for (int j = 0; j < size; j++) {
-                res[i][j] += first[i][k] * second[k][j];
+long calculateParallel(double** A, double* b, double* x) {
+    auto start_time = std::chrono::high_resolution_clock::now();
+    // Прямой ход
+    // Перебор строк. Для высчитывания следующих строк нужны законченные предыдущие
+    for (int k = 0; k < N - 1; k++) {
+#pragma omp parallel for
+        // Перебор столбцов. Вычисления каждого столбца независят от других
+        for (int i = k + 1; i < N; i++) {
+            // Вычисление коэффициента от элемента на главной диагонали
+            double coef = A[i][k] / A[k][k];
+            // Вычисление новых значений в каждом столбце выбранной строки, для получения треугольника 0
+            for (int j = k; j < N; j++) {
+                A[i][j] -= coef * A[k][j];
             }
+            // Изменение значений правой части в соответсвии со строкой
+            b[i] -= coef * b[k];
         }
     }
+    // Обратный ход
+    // Перебор строк с конца. Для каждой следующей строки нужна предыдущая
+    for (int i = N - 1; i >= 0; i--) {
+        // Подсчет суммы левой части, от уже известных значений x (не считая самой первой рассматриваемой строки)
+        double sum = 0;
+        for (int j = i + 1; j < N; j++) {
+            sum += A[i][j] * x[j];
+        }
+        // Вычисление значения x
+        x[i] = (b[i] - sum) / A[i][i];
+    }
+    auto end_time = std::chrono::high_resolution_clock::now();
 
-    auto end = std::chrono::high_resolution_clock::now();
-    return std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
+    return long(duration.count());
+}
+long calculateSequential(double** A, double* b, double* x) {
+    auto start_time = std::chrono::high_resolution_clock::now();
+    // Прямой ход
+    // Перебор строк. Для высчитывания следующих строк нужны законченные предыдущие
+    for (int k = 0; k < N - 1; k++) {
+        // Перебор столбцов. Вычисления каждого столбца независят от других
+        for (int i = k + 1; i < N; i++) {
+            // Вычисление коэффициента от элемента на главной диагонали
+            double coef = A[i][k] / A[k][k];
+            // Вычисление новых значений в каждом столбце выбранной строки, для получения треугольника 0
+            for (int j = k; j < N; j++) {
+                A[i][j] -= coef * A[k][j];
+            }
+            // Изменение значений правой части в соответсвии со строкой
+            b[i] -= coef * b[k];
+        }
+    }
+    // Обратный ход
+    // Перебор строк с конца. Для каждой следующей строки нужна предыдущая
+    for (int i = N - 1; i >= 0; i--) {
+        // Подсчет суммы левой части, от уже известных значений x (не считая самой первой рассматриваемой строки)
+        double sum = 0;
+        for (int j = i + 1; j < N; j++) {
+            sum += A[i][j] * x[j];
+        }
+        // Вычисление значения x
+        x[i] = (b[i] - sum) / A[i][i];
+    }
+    auto end_time = std::chrono::high_resolution_clock::now();
+
+    // Вывод времени выполнения
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
+    return long(duration.count());
 }
 
-inline std::chrono::microseconds multiplyLinearJIK(int** first, int** second, int** res)
-{
-    auto start = std::chrono::high_resolution_clock::now();
+int main() {
 
-    for (int j = 0; j < size; j++) {
-        for (int i = 0; i < size; i++) {
-            for (int k = 0; k < size; k++) {
-                res[i][j] += first[i][k] * second[k][j];
-            }
-        }
-    }
+    double** A = new double* [N];
+    double* b = new double[N];
+    double* x = new double[N];
 
-    auto end = std::chrono::high_resolution_clock::now();
-    return std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    initArrays(A, b, x);
+
+    std::cout << "Result paralleled: " << calculateParallel(A, b, x) << " microseconds" << std::endl;
+    std::cout << "Result sequential: " << calculateSequential(A, b, x) << " microseconds" << std::endl;
+
+    eraseArrays(A, b, x);
+    return 0;
 }
 
-inline std::chrono::microseconds multiplyLinearJKI(int** first, int** second, int** res)
-{
-    auto start = std::chrono::high_resolution_clock::now();
-
-    for (int j = 0; j < size; j++) {
-        for (int k = 0; k < size; k++) {
-            for (int i = 0; i < size; i++) {
-                res[i][j] += first[i][k] * second[k][j];
+void initArrays(double** A, double* b, double* x) {
+    for (int i = 0; i < N; i++) {
+        A[i] = new double[N];
+        for (int j = 0; j < N; j++) {
+            if (i == j) {
+                A[i][j] = 2;
+            }
+            else if (i == j + 1 || i == j - 1) {
+                A[i][j] = -1;
+            }
+            else {
+                A[i][j] = 0;
             }
         }
+        b[i] = i + 1;
     }
 
-    auto end = std::chrono::high_resolution_clock::now();
-    return std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    for (int i = 0; i < N; i++) {
+        x[i] = 0;
+    }
 }
-
-
-inline std::chrono::microseconds multiplyLinearKJI(int** first, int** second, int** res)
-{
-    auto start = std::chrono::high_resolution_clock::now();
-
-    for (int k = 0; k < size; k++) {
-        for (int j = 0; j < size; j++) {
-            for (int i = 0; i < size; i++) {
-                res[i][j] += first[i][k] * second[k][j];
-            }
-        }
-    }
-
-    auto end = std::chrono::high_resolution_clock::now();
-    return std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-}
-
-int main()
-{
-#pragma region Инициализация массивов
-    int** A = new int* [size];
-    int** B = new int* [size];
-    int** C = new int* [size];
-
-    for (int i = 0; i < size; i++) {
-        A[i] = new int[size];
-        B[i] = new int[size];
-        C[i] = new int[size];
-    }
-#pragma endregion
-
-#pragma region Заполнение массивов
-    // перебор строк
-    for (int i = 0; i < size; i++) {
-        // перебор столбцов
-        for (int j = 0; j < size; j++) {
-            A[i][j] = rand() % 500 - 250;
-            B[i][j] = rand() % 500 - 250;
-        }
-    }
-#pragma endregion
-
-    std::cout << "OpenMP:\t\t" << multiplyOpenMP(A, B, C).count() << " microseconds" << std::endl;
-    resetMatrix(C);
-    std::cout << "Sequential:\t" << multiplyLinearIJK(A, B, C).count() << " microseconds" << std::endl;
-    resetMatrix(C);
-    /*std::cout << "IKJ result:\t" << multiplyLinearIKJ(A, B, C).count() << " microseconds" << std::endl;
-    resetMatrix(C);
-    std::cout << "JIK result:\t" << multiplyLinearJIK(A, B, C).count() << " microseconds" << std::endl;
-    resetMatrix(C);
-    std::cout << "JKI result:\t" << multiplyLinearJKI(A, B, C).count() << " microseconds" << std::endl;
-    resetMatrix(C);*/
-    std::cout << "Optimal seq:\t" << multiplyLinearKIJ(A, B, C).count() << " microseconds" << std::endl;
-    resetMatrix(C);
-    /*std::cout << "KJI result:\t" << multiplyLinearKJI(A, B, C).count() << " microseconds" << std::endl;
-    resetMatrix(C);*/
-
-#pragma region Удаление массивов
-    for (int i = 0; i < size; i++) {
+void eraseArrays(double** A, double* b, double* x) {
+    for (int i = 0; i < N; i++) {
         delete[] A[i];
-        delete[] B[i];
-        delete[] C[i];
     }
-
     delete[] A;
-    delete[] B;
-    delete[] C;
-#pragma endregion
-}
-
-// Вывод матрицы с размером [size]
-void print(int** array) {
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            std::cout << array[i][j] << "\t";
-        }
-        std::cout << std::endl;
-    }
-}
-
-inline std::chrono::microseconds multiplyLinearKIJ(int** first, int** second, int** res)
-{
-    auto start = std::chrono::high_resolution_clock::now();
-
-    for (int k = 0; k < size; k++) {
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                res[i][j] += first[i][k] * second[k][j];
-            }
-        }
-    }
-
-    auto end = std::chrono::high_resolution_clock::now();
-    return std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-}
-
-inline std::chrono::microseconds multiplyLinearIJK(int** first, int** second, int** res)
-{
-    auto start = std::chrono::high_resolution_clock::now();
-    
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            for (int k = 0; k < size; k++) {
-                res[i][j] += first[i][k] * second[k][j];
-            }
-        }
-    }
-
-    auto end = std::chrono::high_resolution_clock::now();
-    return std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-}
-
-inline std::chrono::microseconds multiplyOpenMP(int** first, int** second, int** res)
-{
-    int threadsNum = 2;
-    omp_set_num_threads(threadsNum);
-    int i, j, k;
-    auto start = std::chrono::high_resolution_clock::now();
-#pragma omp parallel for shared(first, second, res) private(i, j, k)
-
-    
-    for (i = 0; i < size; i++) {
-        for (j = 0; j < size; j++) {
-            for (k = 0; k < size; k++) {
-                res[i][j] += first[i][k] * second[k][j];
-            }
-        }   
-    }
-
-    auto end = std::chrono::high_resolution_clock::now();
-    return std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-}
-
-inline void resetMatrix(int** matrix) {
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            matrix[i][j] = 0;
-        }
-    }
+    delete[] b;
+    delete[] x;
 }
